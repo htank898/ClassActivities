1bd5273132772a07d0b417d83d46bceda3f1ff64
htank898 <htank898@gmail.com>
2023-09-01 22:25:11 -0500

    push

---
+++ Week2/2.2/2.2.2-JSVariablesAndPrimatives.js
@@ -0,0 +1,34 @@
+// create a variable called name
+let name = "Tim";
+//favorite song
+let favoriteSong = "The Message";
+// Create a variable "waage that references" a Number
+let wage = 10000;
+//Create a variable called "age" that references a number
+let age = 25;
+//create a variable called "onlyChild" that references a boolean
+let onlyChild = true;
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+// create a variable called "favoriteThing" that references ANY primitive value
+let favoriteThing = "Apples";
+// print the data type of "favoriteThing"
+console.log(favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "Starboy";
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = "Pears";
+// Happy Birthday!
+// Reassign "age" to a new value
+age = 26
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 10
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("Hi, my name is " + name + " and I am " + age + " years old");
+e
---
+++ Week2/2.2/2.3.5-Loops.js
@@ -0,0 +1,28 @@
+
+
+
+
+// Follow the steps below to create a **while** loop that TOTALS the numbers from 1 to 100
+
+// 1) create a variable to represent the current number
+
+// 2) create a variable to represent the current total
+
+// 3) write a while loop that sums the numbers from 1 to 100
+let currentnumber = 1;
+let currenttotal = 0;
+
+while (currentnumber <= 100) {
+  console.log(currentnumber += currenttotal);
+  currentnumber++;
+}
+
+
+// Now using the same variables you created in steps 1 and 2, create a **for** loops that TOTALS the numbers from 1 to 100
+
+// 1) write a for loop that sums the numbers from 1 to 100
+let total = 0;
+
+for (let cnumber = 1; cnumber <= 100; cnumber++) {
+total += cnumber;
+}
